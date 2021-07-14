6e2816dcd52e427401d43258403e1774b337b20d
Adrian <adrian@zyian.dev>
2021-07-14 13:00:56 -0400

    WIP: playing with endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,34 @@ import axios from 'axios';
 
 import ItemTable from './items/ItemTable';
 
+interface SalvageItem {
+  id: number,
+  name: string,
+  lastUpdate: string
+}
+
 function App() {
   // Create the count state.
   const [keys, setKeys] = useState<string[]>([]);
+  const [salvages, setSalvages] = useState<SalvageItem[]>([]);
   // Return the App component.
 
   useEffect(() => {
-    if (keys.length > 0) return;
-    axios.get('https://api.silveress.ie/gw2/v1/items/keys').then(resp => {
-      const keys: string[] = [];
-      Object.keys(resp.data).forEach((v) => keys.push(resp.data[v]))
-      setKeys(keys);
-    })
+    if (keys.length < 1) {
+      axios.get('https://api.silveress.ie/gw2/v1/items/keys').then(resp => {
+        const keys: string[] = [];
+        Object.keys(resp.data).forEach((v) => keys.push(resp.data[v]))
+        setKeys(keys);
+      })
+    }
+
+    if (salvages.length < 1) {
+      axios.get('https://api.silveress.ie/gw2/v1/items/json?filter=description:Salvage Item').then(resp => {
+        const salvs: SalvageItem[] = [];
+        Object.keys(resp.data).forEach((v) => salvs.push(resp.data[v]))
+        setSalvages(salvs);
+      })
+    }
   })
 
   return (
@@ -25,14 +41,13 @@ function App() {
         <div>
           <span>This is the list of keys: </span>
           <select name={"keys"} id={"table_keys"} className={"text-gray-600 w-96"}>
-            {keys.length > 1 ? keys.sort().map(k =>
-              <option key={k} value={k}>{k}</option>
+            {salvages.length > 1 ? salvages.map(k =>
+              <option key={`${k.id}+${k.name}`} value={k.name}>{k.name}</option>
             ) : <option>Loading...</option>}
           </select>
         </div>
       </header>
       <section>
-        <ItemTable />
       </section>
     </div>
   );
--- src/items/ItemTable.tsx
+++ src/items/ItemTable.tsx
@@ -1,17 +1,62 @@
-import React, {Fragment, useState} from 'react';
-
+import React, {Fragment, useEffect, useState} from 'react';
 import axios from 'axios';
 
 type Item = {
   id: number;
   buy_price: number;
-
+  buy_quantity: number;
+  lastUpdate: string;
+  name: string;
+  sell_price: number;
+  sell_quantity: number;
 }
 
 function ItemTable() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState<Item[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
-  return <Fragment />;
+  useEffect(() => {
+    if (loading)
+      axios.get<Item[]>('https://api.datawars2.ie/gw2/v1/items/json')
+        .then(resp => {
+          setItems(resp.data.slice(0, 10))
+        })
+        // .catch(err => console.log(err))
+        .finally(() => setLoading(false))
+  })
+
+  return (
+    <div>
+      <table>
+        <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Buy Price</th>
+          <th>Buy Quantity</th>
+          <th>Sell Price</th>
+          <th>Sell Quantity</th>
+          <th>Last Updated</th>
+        </tr>
+        </thead>
+        <tbody>
+        {items.map(i => {
+          return (
+            <tr key={i.id}>
+              <td>{i.id}</td>
+              <td>{i.name}</td>
+              <td>{i.buy_price}</td>
+              <td>{i.buy_quantity}</td>
+              <td>{i.sell_price}</td>
+              <td>{i.sell_quantity}</td>
+              <td>{i.lastUpdate}</td>
+            </tr>
+          )
+        })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default ItemTable;
